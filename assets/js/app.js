653f3ae18e973d1d521f887f0f25a9cb2054a0bf
Enrique Tafoya Prieto <enrique.tafoya@hotmail.com>
2021-10-22 14:56:18 -0500

    *Agregamos la barra lateral de búsqueda.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,21 +3,30 @@
     $(document).ready(function(evt){
 
         var app = {
-            init: function() {
+            $contentBlockButtons: null,
+            $cleanSearchBoxButton: null,
+            $searchBox: null,
+            totalContentBlockButtons: 0,
+            init: function() {                
+                app.$contentBlockButtons = $(".content-block-button");
+                app.totalContentBlockButtons = app.$contentBlockButtons.length;
+                app.$searchBox = $("#searchBox");
+                app.$cleanSearchBoxButton = $("#cleanSearchBoxButton");                
+
                 this.enableSmoothScroll();
                 this.enableCodeHighlighting();
+                this.enableSearchBox();
+                this.enableCleanSearchBox();
             }, //init
             enableSmoothScroll: function(){
-
-                var $contentBlockButtons = $(".content-block-button");
                 var $html = $("html");
 
                 //Display content blocks
-                $contentBlockButtons.on("click", function(evt){  
+                app.$contentBlockButtons.on("click", function(evt){  
 
                     evt.preventDefault();
 
-                    $contentBlockButtons.removeClass("active");
+                    app.$contentBlockButtons.removeClass("active");
                     evt.target.className += " active";
 
                     if (this.hash !== null && this.hash !== ""){                
@@ -28,17 +37,50 @@
                         }, 800);
                     }//if this.hash      
                     
-
                     return false;
 
                  });//contentBlockButtons.on
             },//enableSmoothScroll
             enableCodeHighlighting: function () {
-
                 hljs.highlightAll();
-
-            }//enableCodeHighlighting
-
+            },//enableCodeHighlighting
+            enableSearchBox: function(){                
+                app.$searchBox.keyup(function(evt){
+                    var input = this.value.trim().toLowerCase();
+                    var inputLength = input.length;
+                    if (inputLength > 0) {
+                        app.showCleanSearchBoxButton();
+                        for(var i = 0; i < app.totalContentBlockButtons ; i++) {
+                            var iText = app.$contentBlockButtons[i].innerHTML.toLowerCase();                            
+                            if (iText.indexOf(input) === -1){
+                                app.$contentBlockButtons[i].style.display = "none";
+                            } else {
+                                app.$contentBlockButtons[i].style.display = "block";
+                            }
+                        }//for
+                    } else {
+                        app.hideCleanSearchBoxButton();
+                        app.showContentBlockButtons();
+                    }
+                });//$searchBox.keydown                
+            },//enableSearchBox
+            enableCleanSearchBox: function(){
+                this.$cleanSearchBoxButton.click(function(evt){
+                    app.$searchBox.val("");
+                    app.showContentBlockButtons();
+                    app.hideCleanSearchBoxButton();
+                });
+            },//enableCleanSearchBox
+            showContentBlockButtons: function(){
+                for (var i = 0; i < this.totalContentBlockButtons ; i++)
+                    this.$contentBlockButtons[i].style.display = "block";
+            },//showContentBlockButtons
+            showCleanSearchBoxButton: function(){
+                this.$cleanSearchBoxButton.show();
+            },//showCleanSearchBoxButton
+            hideCleanSearchBoxButton: function(){
+                this.$cleanSearchBoxButton.hide();
+            },//hideCleanSearchBoxButton
         };//app;
 
         app.init();
